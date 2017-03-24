58127d65f5eccd2d2ac776e9137bfc5440fb7019
Bragalund <bakhen15@student.westerdals.no>
2017-03-24 17:17:32 +0100

    Har laget en package kalt Score med en java-klasse inne i. Den extender AppCompat og setter den nye xml-filen(score_screen) som View.

---
+++ app/src/main/java/bakhen/no/tictactoe3/Score/ScoreScreen.java
@@ -0,0 +1,16 @@
+package bakhen.no.tictactoe3.Score;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import bakhen.no.tictactoe3.R;
+
+public class ScoreScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score_screen);
+    }
+}
