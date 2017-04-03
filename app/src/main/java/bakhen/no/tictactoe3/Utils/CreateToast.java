58bc1c67699e684d3aed994213ace833e2d034cf
Bragalund <bakhen15@student.westerdals.no>
2017-04-03 19:03:23 +0200

    Har lagt til funksjonalitet for å validere brukernavnet til spiller nummer 2 i CreateToast-klassen i utils-packagen.

--- app/src/main/java/bakhen/no/tictactoe3/Utils/CreateToast.java
+++ app/src/main/java/bakhen/no/tictactoe3/Utils/CreateToast.java
@@ -5,14 +5,14 @@ import android.widget.Toast;
 
 public class CreateToast {
     public static boolean validateText(Context context, String username, String secondUsername) {
-        if (username.length() < 1) {
+        if (username.length() < 1 || secondUsername.length() < 1) {
             CreateToast.createToast(context, "Username is to short.");
             return false;
-        } else if (username.length() > 15) {
+        } else if (username.length() > 15 || secondUsername.length() > 15) {
             createToast(context, "username to long.");
             return false;
         }else if(username.equals(secondUsername)){
-            createToast(context, "Username cannot be the same as player 1");
+            createToast(context, "Usernames cannot be the same.");
             return false;
         }
         return true;
